c12b42658d9401089c1ff83aef6038379c50b123
13537223157@163.com <13537223157@163.com>
2023-11-21 00:39:43 +0800

    feature: optimization loading component

--- src/utils/loading/index.tsx
+++ src/utils/loading/index.tsx
@@ -1,23 +1,36 @@
-import ReactDOM from "react-dom/client";
-import Loading from "./loading";
-
+import "./index.scss";
 let count = 0;
+
+// export const showLoading = () => {
+//   if (count === 0) {
+//     const loading = document.createElement("div");
+//     loading.setAttribute("id", "loading");
+//     ReactDOM.createRoot(loading).render(<Loading />);
+//   }
+//   count++;
+// };
+
+// export const hideLoading = () => {
+//   if (count < 0) return;
+//   count--;
+//   if (count === 0) {
+//     document.body.removeChild(
+//       document.getElementById("loading") as HTMLElement,
+//     );
+//   }
+// };
 
 export const showLoading = () => {
   if (count === 0) {
-    const loading = document.createElement("div");
-    loading.setAttribute("id", "loading");
-    ReactDOM.createRoot(loading).render(<Loading />);
+    const loading = document.getElementById("loading");
+    loading?.style.setProperty("display", "flex");
   }
   count++;
 };
-
 export const hideLoading = () => {
-  if (count < 0) return;
   count--;
   if (count === 0) {
-    document.body.removeChild(
-      document.getElementById("loading") as HTMLElement,
-    );
+    const loading = document.getElementById("loading");
+    loading?.style.setProperty("display", "none");
   }
 };
